f9850ac660bad7dc9bb9aa56d855726b232ce18e
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-01 01:35:54 +0700

    Update code

--- script/js/script.js
+++ script/js/script.js
@@ -3,4 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const body = document.body;
     
     body.classList.add('bg-l4');
+
+    const themeToggler = document.getElementById('theme');
+
+    if (themeToggler.checked) {
+        body.classList.remove('bg-l4');
+        body.classList.add('bg-d1');
+    } else {
+        body.classList.remove('bg-d1');
+        body.classList.add('bg-l4');
+    }
 });
