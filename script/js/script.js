8378f04dd9a37ee07236867e629da6df676ccf1f
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-01 12:42:38 +0700

    Update + Add md component, add utils, optimizing theme using selector with appropriate variables, testing JS functionalities, etc.

--- script/js/script.js
+++ script/js/script.js
@@ -1,16 +1,37 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    const body = document.body;
-    
-    body.classList.add('bg-l4');
-
-    const themeToggler = document.getElementById('theme');
-
-    if (themeToggler.checked) {
-        body.classList.remove('bg-l4');
-        body.classList.add('bg-d1');
-    } else {
-        body.classList.remove('bg-d1');
-        body.classList.add('bg-l4');
+    function saveThemePreferences(theme) {
+        localStorage.setItem('theme', theme);
     }
+
+    function setTheme(themeName) {
+        saveThemePreferences(themeName);
+
+        document.documentElement.className = themeName;
+    }
+
+    function toggleTheme() {
+        if (localStorage.getItem('theme') === 'light-theme') {
+            setTheme('light-theme');
+        } else {
+            setTheme('dark-theme');
+        }
+    }
+
+    (function () {
+        if (localStorage.getItem('theme') === 'dark-theme') {
+            setTheme('dark-theme');
+        } else {
+            setTheme('light-theme');
+        }
+    })();
+
+    const themeToggler = document.getElementById('themeToggler');
+    themeToggler.addEventListener('click', () => {
+        if (themeToggler.checked) {
+            toggleTheme();
+        } else {
+            toggleTheme();
+        }
+    });
 });
