0d34e359b407611385fc9eb2f507727ae066d263
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-02 20:26:05 +0700

    npm-ized project

---
+++ liveServer.js
@@ -0,0 +1,14 @@
+
+const { enable } = require("colors");
+const liveServer = require("live-server");
+
+const params = {
+  port: 9800,
+  host: "127.0.0.1",
+  root: "./",
+  open: true,
+  wait: 1200,
+  cors: enable,
+};
+
+liveServer.start(params);
