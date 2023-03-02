36f6856bc47e9e0086251431b8d620039262389c
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-03-02 18:39:40 +0700

    Add Utils and set content delay display time

--- script/js/utils/loader.js
+++ script/js/utils/loader.js
@@ -2,6 +2,14 @@
 function Loader() {
   document.body.classList.add("overflow-hidden");
 
+  const _header = document.querySelector('header');
+  const _main = document.querySelector('main');
+  const _footer = document.querySelector('footer');
+
+  _header.classList.add('display-none');
+  _main.classList.add('display-none');
+  _footer.classList.add('display-none');
+
   document.onreadystatechange = function () {
     const loader = document.getElementById("loader");
 
@@ -12,6 +20,12 @@ function Loader() {
     } else {
       document.body.classList.remove("overflow-hidden");
       document.body.style.display = "block";
+
+      setTimeout(() => {
+        _header.classList.remove('display-none');
+        _main.classList.remove('display-none');
+        _footer.classList.remove('display-none');
+      }, 600);
 
       loader.style.display = "none";
     }
